06bb6ac338df22899fb532a79b534c554a0b3b84
1714290024 <1714290024@SJB6-D40.academico.iesb.br>
2018-05-11 21:20:39 -0300

    Implementação da classe UnidadeController e Route

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,13 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+Route:: group(['prefix' => 'unidade'], function(){
+    Route::get('listar', 'UnidadeController@listar');
+    Route::get('criar', 'UnidadeController@criar');
+    Route::get('(id)/editar', 'UnidadeController@editar');
+    Route::get('(id)/remover', 'UnidadeController@remover');
+    Route::get('salvar', 'UnidadeController@salvar');
+});
