26618baaccd2a75d125e3c66fe9f74efbd452cb4
1714290019 <1714290019@SJB6-D38.academico.iesb.br>
2018-05-11 21:05:38 -0300

    Implentação da classe AssuntoController.

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,16 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route:: group(['prefix' => 'assunto'], function(){
+    Route::get('listar', 'AssuntoController@listar');
+    Route::get('criar', 'AssuntoController@criar');
+    Route::get('(id)/editar', 'AssuntoController@editar');
+    Route::get('(id)/remover', 'AssuntoController@remover');
+    Route::get('salvar', 'AssuntoController@salvar');
+});
