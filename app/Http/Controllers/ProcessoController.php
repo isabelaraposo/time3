c6986fe5440efd800f832c9198f762294dcb0525
isabelaraposo <isabelaraposo@users.noreply.github.com>
2018-06-24 20:43:26 -0300

    Merge origin/master into 1624290021
    Conflicts:
    	routes/web.php

--- app/Http/Controllers/ProcessoController.php
+++ app/Http/Controllers/ProcessoController.php
@@ -4,45 +4,53 @@ namespace App\Http\Controllers;
 
 use App\Processo;
 use Illuminate\Http\Request;
+use App\Http\requests\ProcessoRequest;
 
 class ProcessoController extends Controller
 {
+    public function _construct(){
+       //$this->middleware('auth');
+    }
     public function listar() {
-        return Processo::all();
+        return view('processo.listar', ['processos' => Processo::paginate(5)]);
+            }
         
-    }
         public function criar() {
-            
+            return view('processo.criar' );
         
     }
-        public function editar($id) {
-            return Processo::find($id);
+         public function editar($id){ 
+
+        return view('processo.editar',['processos' => Processo::find($id)]);
         
     }
-        public function remover() {
+    
+        public function remover($id) {
             $processo = Processo::find($id);
             $processo->delete();
-            return redirect('Processo/listar');
+            return redirect('processo/listar');
         
     }
-        public function salvar(Request $request) {
-            $processo = new Processo();
-            
-            if($request->has('id')){
-                $processo = Processo::fid($id);
-            }
-            
-            $processo->cod_processo = $request->cod_processo;
-            $processo->descricao = $request->descricao;
-            $processo->data = $request->data;
-            $processo->situacao = $request->situacao;
-            $processo->assunto = $request->assunto;
-            $processo->arquivo = $request->arquivo;
-            $processo->unidade = $request->unidade;
+    public function salvar(ProcessoRequest $request) {
+        $processo = new Processo();
+        $hasprocesso = Processo::find($request->input('id'));
+        if ($hasprocesso != null){
+            $processo = Processo::find($request->input('id'));
+            $processo->cod_processo = $request->input('cod_processo');
+            $processo->descricao = $request->input('descricao');
+            $processo->data = $request->input('data');
+            $processo->situacao = $request->input('situacao');
+            $processo->assunto = $request->input('assunto');
+            $processo->arquivo = $request->input('arquivo');
+            $processo->unidade = $request->input('unidade');
             $processo->save();
+        }
+
             
-            return('processo/listar');
+            return redirect ('processo/listar');
+         
             
-        
-    }
+           }
 }
+
+
